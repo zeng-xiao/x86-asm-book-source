18cbd6857ace29b79fd97dda3d2126f32b61d257
zengxiao <zengxiao@eswin.com>
2020-11-20 22:08:44 +0800

    change Makefile to product img for bochs

--- c05/c05_mbr.asm
+++ c05/c05_mbr.asm
@@ -1,38 +1,29 @@
-         ;代码清单5-1 
-         ;文件名：c05_mbr.asm
-         ;文件说明：硬盘主引导扇区代码
-         ;创建日期：2011-3-31 21:15 
-         
          mov ax,0xb800                 ;es寄存器指向文本模式的显示缓冲区
          mov es,ax
 
-         ;以下显示字符串"Label offset:"
-         ;字符的显示属性分为两个字节
-         ;第一个字节是字符的ASCII码，第二个字节是字符的显示属性
-         ;下面的0x07表示字符以黑底白字，无闪烁无加亮的方式显示
-         mov byte [es:0x00],'L'
+         mov byte [es:0x00],'z'
          mov byte [es:0x01],0x07
-         mov byte [es:0x02],'a'
+         mov byte [es:0x02],'e'
          mov byte [es:0x03],0x07
-         mov byte [es:0x04],'b'
+         mov byte [es:0x04],'n'
          mov byte [es:0x05],0x07
-         mov byte [es:0x06],'e'
+         mov byte [es:0x06],'g'
          mov byte [es:0x07],0x07
-         mov byte [es:0x08],'l'
+         mov byte [es:0x08],' '
          mov byte [es:0x09],0x07
-         mov byte [es:0x0a],' '
+         mov byte [es:0x0a],'x'
          mov byte [es:0x0b],0x07
-         mov byte [es:0x0c],"o"
+         mov byte [es:0x0c],"i"
          mov byte [es:0x0d],0x07
-         mov byte [es:0x0e],'f'
+         mov byte [es:0x0e],'a'
          mov byte [es:0x0f],0x07
-         mov byte [es:0x10],'f'
+         mov byte [es:0x10],'o'
          mov byte [es:0x11],0x07
-         mov byte [es:0x12],'s'
+         mov byte [es:0x12],' '
          mov byte [es:0x13],0x07
-         mov byte [es:0x14],'e'
+         mov byte [es:0x14],' '
          mov byte [es:0x15],0x07
-         mov byte [es:0x16],'t'
+         mov byte [es:0x16],' '
          mov byte [es:0x17],0x07
          mov byte [es:0x18],':'
          mov byte [es:0x19],0x07
@@ -40,13 +31,9 @@
          mov ax,number                 ;取得标号number的偏移地址
          mov bx,10                     ;bx保存被除数,div指令使用bx寄存器的值作为被除数
 
-         ;设置数据段的基地址
          mov cx,cs
          mov ds,cx
 
-         ;32位除法中，被除数的低16位在ax寄存器中，高16位在dx寄存器中
-         ;前面已经将number的地址赋值给ax，下面将dx清零
-         ;求个位上的数字
          mov dx,0
          div bx
          mov [0x7c00+number+0x00],dl   ;保存个位上的数字
@@ -56,7 +43,6 @@
          div bx
          mov [0x7c00+number+0x01],dl   ;保存十位上的数字
 
-         ;求百位上的数字
          xor dx,dx
          div bx
          mov [0x7c00+number+0x02],dl   ;保存百位上的数字
@@ -66,12 +52,10 @@
          div bx
          mov [0x7c00+number+0x03],dl   ;保存千位上的数字
 
-         ;求万位上的数字 
          xor dx,dx
          div bx
          mov [0x7c00+number+0x04],dl   ;保存万位上的数字
 
-         ;以下用十进制显示标号的偏移地址
          mov al,[0x7c00+number+0x04]    ;将计算结果送到al寄存器中
          add al,0x30                    ;加上0x30得到这个数字的ASCII码
          mov [es:0x1a],al               ;得到的ASCII码送到指定的位置
